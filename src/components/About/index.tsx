131837b4bba51999143deae3540a3e9fb4bbd958
Mohammad Hosein <mhmighani.dev@gmail.com>
2024-01-19 17:02:51 +0330

    chore: update about

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,7 +1,7 @@
 import styles from "./styles.module.scss";
 
 const ABOUT =
-  "I’m a Web Developer who has mainly focused on developing the Front-End part of web applications. I constantly seek for . In addition to JavaScript ES6 and React, I have experience working with Python.";
+  "I’m a Web Developer who has mainly focused on developing the Front-End part of web applications. I care about reusability and Best Practices in my coding style. In addition to JavaScript ES6 and React, I have experience working with Python.";
 
 function About() {
   return (
