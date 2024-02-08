98ea2af05c149a7f06ddfad24650a9da7a80c97c
Mohammad Hosein <mhmighani.dev@gmail.com>
2024-02-08 23:39:32 +0330

    wip: experiences section

--- src/components/Experiences/index.jsx
+++ src/components/Experiences/index.jsx
@@ -1,5 +1,24 @@
+import ExperienceCard from "./components/ExperienceCard";
+
+const EXPERIENCES = [
+  {
+    id: "raychat",
+    expTitle: "Front-End Developer",
+    expSubTitle: "Raychat",
+    expLogoAddress:
+      "https://raychat.io/_next/static/media/raychat-logo.ef48f641.svg",
+    achievements: ["one", "two", "three"],
+    duration: { from: "Jul 2022", to: "Present" },
+    about: "A customer service chat service",
+  },
+];
+
 function Experiences() {
-  return <div>Experiences</div>;
+  const renderExps = EXPERIENCES.map((exp) => (
+    <ExperienceCard key={exp.id} {...exp} />
+  ));
+
+  return <div>{renderExps}</div>;
 }
 
 export default Experiences;
