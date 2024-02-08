3f315c8fb1698584b65200f63a181c0900a4f3c9
Mohammad Hosein <mhmighani.dev@gmail.com>
2024-02-08 23:34:07 +0330

    wip: experiences section

--- src/components/Experiences/components/ExperienceCard/index.tsx
+++ src/components/Experiences/components/ExperienceCard/index.tsx
@@ -1,8 +1,14 @@
+import Image from "next/image";
+
+import styles from "./styles.module.scss";
+
 type Props = {
   expTitle: string;
   expSubTitle: string;
   expLogoAddress: string;
   duration: { from: string; to: string };
+  achievements: string[];
+  about: string;
 };
 
 function ExperienceCard({
@@ -10,8 +16,44 @@ function ExperienceCard({
   expSubTitle,
   expLogoAddress,
   duration,
+  achievements,
+  about,
 }: Props) {
-  return <div></div>;
+  const renderAchievements = achievements.map((item, index) => (
+    <li key={index}>{item}</li>
+  ));
+
+  return (
+    <div className={styles.exp_card}>
+      {/* title */}
+      <div className={styles.exp_card__header}>
+        {/* info */}
+        <div className={styles.exp_card__header__title_container}>
+          <Image
+            width={65}
+            height={65}
+            alt={expSubTitle}
+            src={expLogoAddress}
+          />
+          <div className={styles.exp_card__header__title}>
+            <h3>{expTitle}</h3>
+            <span>{expSubTitle}</span>
+          </div>
+        </div>
+
+        {/* period */}
+        <div>
+          <span>{duration.from}</span>
+          <span>{duration.to}</span>
+        </div>
+      </div>
+
+      {/* achivement content */}
+      <ul>{renderAchievements}</ul>
+
+      <p>{about}</p>
+    </div>
+  );
 }
 
 export default ExperienceCard;
--- src/stories/ExperienceCard.stories.tsx
+++ src/stories/ExperienceCard.stories.tsx
@@ -10,6 +10,18 @@ export default meta;
 
 type Story = StoryObj<typeof ExperienceCard>;
 
+const achievements = ["one", "two", "three"];
+
+const about = "A customer service chat service";
+
 export const Normal: Story = {
-  args: {},
+  args: {
+    expTitle: "Front-End Developer",
+    expSubTitle: "Raychat",
+    expLogoAddress:
+      "https://raychat.io/_next/static/media/raychat-logo.ef48f641.svg",
+    achievements,
+    duration: { from: "Jul 2022", to: "Present" },
+    about,
+  },
 };
