f850ffba64713c90ba45c04dc8a2693d4cdc2ea1
Mohammad Hosein <mhmighani.dev@gmail.com>
2024-02-16 22:47:50 +0330

    chore: update content

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,7 +1,7 @@
 import styles from "./styles.module.scss";
 
 const ABOUT =
-  "I’m a Web Developer who has mainly focused on developing the Front-End part of web applications. I care about reusability and Best Practices in my coding style. In addition to JavaScript ES6 and React, I have experience working with Python.";
+  "I'm a Front End Developer with Proficiency in core Front-End technologies and frameworks such as JavaScript, TypeScript, HTML, CSS, React.js, Redux, and Next.js. I prioritize writing clean and maintainable code in my coding style and thrive on creating visually compelling products.In addition to my expertise in Front-End development, I have some experiences doing scripting and automation tasks with Python.";
 
 function About() {
   return (
--- src/components/Experiences/index.jsx
+++ src/components/Experiences/index.jsx
@@ -7,9 +7,15 @@ const EXPERIENCES = [
     expSubTitle: "Raychat",
     expLogoAddress:
       "https://raychat.io/_next/static/media/raychat-logo.ef48f641.svg",
-    achievements: ["one", "two", "three"],
+    achievements: [
+      "-	Participated in optimizations such as improving loading times by implementing lazy loading techniques, code splitting and rendering optimization.",
+      "-	Participated in adding multilingual support to services.",
+      "-	Participated in refactoring legacy code, and extracting reusable components resulting in improved maintainability.",
+      "-	Implemented multiple features such as Raychat's answering bot.",
+      "-	Participated in documenting services using Confluence.",
+    ],
     duration: { from: "Jul 2022", to: "Present" },
-    about: "A customer service chat service",
+    about: "A customer service chat platform",
     expExternalLink: "https://raychat.io/",
   },
 ];
