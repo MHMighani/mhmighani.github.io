d017adfb63e09da7e6951d69dd611e12719eea8f
Mohammad Hosein <mhmighani.dev@gmail.com>
2024-03-01 11:54:36 +0330

    fix: style

--- src/components/NavigationSection/index.tsx
+++ src/components/NavigationSection/index.tsx
@@ -8,12 +8,7 @@ function NavigationSection() {
   return (
     <section className={styles.navigation_section}>
       <div className={styles.profile_and_contact}>
-        <Image
-          className={styles.profile_image}
-          width={150}
-          src={introImage}
-          alt="me"
-        />
+        <Image className={styles.profile_image} src={introImage} alt="me" />
         <div className={styles.name_contact}>
           <h2 className={styles.name}>Mohammad Hossein Mighani</h2>
           <Reachme />
