d38741dc0e0b6733dde8e988b2ea7ead8def6a6d
Mohammad Hosein <mhmighani.dev@gmail.com>
2024-03-06 13:08:03 +0330

    chore: add typing effect

--- src/components/NavigationSection/index.tsx
+++ src/components/NavigationSection/index.tsx
@@ -1,16 +1,44 @@
+"use client";
+
 import Image from "next/image";
+import { useRef, useEffect } from "react";
+
+import Typed from "typed.js";
 
 import Reachme from "components/Reachme";
 import introImage from "assets/images/intro_image.jpg";
 import styles from "./styles.module.scss";
 
 function NavigationSection() {
+  const typeEffectEl = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(typeEffectEl.current, {
+      strings: [
+        "I'm a Web Developer",
+        "Focused on Front End",
+        "Always trying to learn",
+      ],
+      typeSpeed: 70,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <section className={styles.navigation_section}>
       <div className={styles.profile_and_contact}>
         <Image className={styles.profile_image} src={introImage} alt="me" />
         <div className={styles.name_contact}>
           <h2 className={styles.name}>Mohammad Hossein Mighani</h2>
+
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <div ref={typeEffectEl}></div>
+          </div>
+
           <Reachme />
         </div>
       </div>
