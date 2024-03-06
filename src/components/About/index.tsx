1a1bc1de46c312fc18e17b2306544648fd6b3e6e
Mohammad Hosein <mhmighani.dev@gmail.com>
2024-03-06 11:49:50 +0330

    feat: add colors to readme keywords

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,14 +1,30 @@
 import NeonText from "components/NeonText";
 import styles from "./styles.module.scss";
 
+const COLORS = {
+  HTML: "#e96228",
+  CSS: "#2862e9",
+  JavaScript: "#efd81d",
+  REACT: "#087a9f",
+  TypeScript: "#2f74c0",
+  python: "#f7cc42",
+  next: "#0d90f7",
+  redux: "#0d90f7",
+};
+
 function About() {
-  const renderHtmlWord = <NeonText text="HTML" />;
-  const renderReduxWord = <NeonText text="redux" />;
-  const renderNextWord = <NeonText text="Next.js" />;
-  const renderTypeScript = <NeonText text="TypeScript" />;
-  const renderJavaScript = <NeonText text="JavaScript" />;
-  const renderCssWord = <NeonText text="CSS" />;
-  const renderReactWord = <NeonText text="React.js" />;
+  const renderHtmlWord = <NeonText color={COLORS["HTML"]} text="HTML" />;
+  const renderReduxWord = <NeonText color={COLORS["redux"]} text="redux" />;
+  const renderNextWord = <NeonText color={COLORS["next"]} text="Next.js" />;
+  const renderTypeScript = (
+    <NeonText color={COLORS["TypeScript"]} text="TypeScript" />
+  );
+  const renderJavaScript = (
+    <NeonText color={COLORS["JavaScript"]} text="JavaScript" />
+  );
+  const renderCssWord = <NeonText color={COLORS["CSS"]} text="CSS" />;
+  const renderReactWord = <NeonText color={COLORS["REACT"]} text="React.js" />;
+  const renderPythonWord = <NeonText color={COLORS["python"]} text="Python" />;
 
   const ABOUT = (
     <>
@@ -18,7 +34,7 @@ function About() {
       and {renderNextWord}. I prioritize writing clean and maintainable code in
       my coding style and thrive on creating visually compelling products.In
       addition to my expertise in Front-End development, I have some experiences
-      doing scripting and automation tasks with Python.
+      doing scripting and automation tasks with {renderPythonWord}.
     </>
   );
 
--- src/components/NeonText/index.tsx
+++ src/components/NeonText/index.tsx
@@ -1,9 +1,13 @@
 import styles from "./styles.module.scss";
 
-type PropsTypes = { text: string };
+type PropsTypes = { text: string; color?: string };
 
-function Neon({ text }: PropsTypes) {
-  return <span className={styles.neon}>{text}</span>;
+function Neon({ text, color }: PropsTypes) {
+  return (
+    <span style={{ color }} className={styles.neon}>
+      {text}
+    </span>
+  );
 }
 
 export default Neon;
